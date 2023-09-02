c596f2d1ff4df3b528958c5677431a327f865f6c
kerexaaa <kerexakerexa@gmail.com>
2023-09-02 11:56:21 +0300

    new

---
+++ firebase-connection.js
@@ -0,0 +1,14 @@
+// web app's firebase config
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBTJhcHkl83ZJVc1feuKwEaCiz3lmtJNiY",
+    authDomain: "otp1-f2035.firebaseapp.com",
+    projectId: "otp1-f2035",
+    storageBucket: "otp1-f2035.appspot.com",
+    messagingSenderId: "982317853149",
+    appId: "1:982317853149:web:56aadfc02b3c4dc2332e34",
+    measurementId: "G-Z37DK8SKTH"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics()
